c89e34149380c700b465e960722ec3601e8aed0d
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2023-04-18 00:09:16 -0400

    Estruturas RB

---
+++ Ruby OBC/Estruturas de controle/index.rb
@@ -0,0 +1,14 @@
+# if
+# else
+# elseif
+# unless é executado apenas se a condição for falsa
+# case Instrução muito parecida como if. Ele se enquadra muito bem
+#a situações com diversas condições.
+
+day = 'Sunday' 
+ lunch = 'Normal'
+if day == 'Sunday' 
+  lunch = 'special'
+end
+ 
+puts "Lunch is #{lunch} today"
